ba2c37a52c0ce4a403a2f3befb7f338bdc52ff60
Sapar Atayev <sopa.97@mail.ru>
2019-03-13 00:08:55 +0300

    feat: add function mmoney-exchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if(currency > 10000)
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    return {};
 }
